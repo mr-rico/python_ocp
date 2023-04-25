d31529dfe9a4dbed863eccb1d5c11b304a1dfa14
Rico Randall <88862134+mr-rico@users.noreply.github.com>
2023-04-25 17:44:17 -0400

    Update cluster_info.py

--- cluster_info.py
+++ cluster_info.py
@@ -1,38 +1,61 @@
 from kubernetes import client
 from kubernetes.client import ApiClient
+from openshift.dynamic import DynamicClient
+from openshift.helper.userpassauth import OCPLoginConfiguration
+import getpass
 
-pod_name = input("Enter the name of the pod to retrieve [default=alertmanager-main-0]: ") or "alertmanager-main-0"
-project_name = input("Enter the name of the project [default=oneview-nextgen-dev]: ") or "openshift-monitoring"
 
-def __get_kubernetes_client(bearer_token,api_server_endpoint):
+pod_search = input("Enter the name of the pod to retrieve: ") or ["alertmanager-main-0", "prometheus-k8s-0", "console-cbd6c889b-226dc"]
+project_search = input("Enter the name of the project': ") or ["openshift-monitoring", "openshift-console"]  
+
+def __get_kubernetes_client(cluster_details):
     try:
-        configuration = client.Configuration()
-        configuration.host = api_server_endpoint
-        configuration.verify_ssl = False
-        configuration.api_key = {"authorization": "Bearer " + bearer_token}
-        client.Configuration.set_default(configuration)
-        client_api = client.CoreV1Api()
-        return client_api
+        print("Collecting credentials for " + cluster_details["api_server_endpoint"])
+        username = input("Enter your username: ") or "default_name"
+        password = getpass.getpass("Enter your password: ")
+        
+        kubeConfig = OCPLoginConfiguration(ocp_username=username, ocp_password=password)
+        kubeConfig.host = cluster_details["api_server_endpoint"]
+        kubeConfig.verify_ssl = True
+        kubeConfig.ssl_ca_cert = cluster_details["ca_cert"] # use a certificate bundle for the TLS validation
+        # Retrieve the auth token
+        kubeConfig.get_token() 
+        k8s_client = client.ApiClient(kubeConfig)      
+        dyn_client = DynamicClient(k8s_client)
+        return dyn_client
+    
     except Exception as e:
         print("Error getting kubernetes client \n{}".format(e))
         return None
 
-def get_pods(cluster_details, namespace=project_name,all_namespaces=False):
-        client_api= __get_kubernetes_client(
-            bearer_token=cluster_details["bearer_token"],
-            api_server_endpoint=cluster_details["api_server_endpoint"],
-        )
-        for i in client_api.list_namespaced_pod(namespace = project_name).items:
-            if i.metadata.name == pod_name:
-                print("%s\t%s\t%s" %
-                (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
-if __name__ == "__main__":
-    cluster1_details={
-        "bearer_token":"eyJhbGciOiJSUzI1NiIsImtpZCI6ImtMR21iVTYtSjE3TU10OWdNMl9zVVVaYmEtNWhjZ2kzX1JhWmIxQldtYWsifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJvcGVuc2hpZnQtbW9uaXRvcmluZyIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJyaWNvLXNhLXRva2VuLWRjNjRqIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6InJpY28tc2EiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiI3ODBmYzk3MS0xMGQ3LTRiMGUtYWFlZS0zMDY2NjBjOWJjYjIiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6b3BlbnNoaWZ0LW1vbml0b3Jpbmc6cmljby1zYSJ9.P145rC1cyeFYOjMeH4w0oU7WWf5eXZgyAISy3gETug7BVqbF746_sc7EMSozVuQm4lfoOvqxp6IiLa2aJx1XIBNg83eXNGryOuXDKvDEsxsjlznmW2YlRtjjFus_0wRKEV80S9nbOro_ILKnpAC0CBVIkyrbjEP44_hwI7o7drKfIhUduyh_HNDv9ohCPxMlkU7MqI6S0m3JNi_Y54HSLUyHeAnpabpJPgrOaFUjdDxAlnHTi3UXBCV1WHGTD4MbVlwPKtW9eoZUYR8mLDei3OEKHP4Nem-vGNUPrTy84-vOUMLBvd4qvE9PvK6918PFECk3XH4mEs6gXPMN6Oj2kPudBSijclfZ5aWtiTs7QgtwlvehFvGwME8bhfJ9iJvAtfPN0RlHtayKWwe49MfgEat6OlJNX6FCAySqEBDcmscXntQslELLpRjtdov4x6NK4pQ_ErPyzckYFBDQ89HYuzwhhXVqz4d0QZEVFXdxGnSupva3ykkvku0dulV4fje95YkbEInVjxXkdrw7jaCl7oiz0gOnpqvveDp0wKlFcdnVrCp_Frb_4qEI4-KYk6Pb1rHRKeC3BbFgKRELJcyCmdUpl3UcgnIdX-EBJ-m2QFuyM-u3IIGEujQ3rW_d_1x4q_Onp8pAELA-A1Pik8TehUnnzn02xzojvPaIcr4r5Mo",
-        "api_server_endpoint":"https://api.ricocluster.lab.upshift.rdu2.redhat.com:6443"
+def get_pods():
+    print("%s\t\t%s\t\t\t%s\t\t\t%s" % 
+    ("POD IP", "PROJECT", "NAME", "CLUSTER"))
 
-    }      
+    for cluster in all_clusters:   
+        v1_pods = cluster["client"].resources.get(api_version='v1', kind='Pod')
+        pod_list = v1_pods.get()       
+        for pod in pod_list.items:
+            if pod.metadata.name in pod_search:
+                print("%s\t%s\t%s\t%s" %
+                (pod.status.podIP, pod.metadata.namespace, pod.metadata.name, cluster["api_server_endpoint"]))  
 
-print("CLUSTER 1 OUTPUT")
-get_pods(cluster1_details)
+
+if __name__  ==  "__main__":
+    cluster1_details = {
+        "ca_cert":"./certificates/cluster1.crt",
+        "api_server_endpoint":"https://api.cluster1.openshift.com:6443",
+        "client":OCPLoginConfiguration(ocp_username=None, ocp_password=None)
+    } 
+    cluster2_details = {
+        "ca_cert":"./certificates/cluster2.crt",
+        "api_server_endpoint":"https://api.cluster2.openshift.com:6443",
+        "client":OCPLoginConfiguration(ocp_username=None, ocp_password=None)
+    } 
+    all_clusters = [cluster1_details, cluster2_details]
+
+for cluster in all_clusters:   
+    cluster["client"] = __get_kubernetes_client(cluster)
+
+get_pods()
